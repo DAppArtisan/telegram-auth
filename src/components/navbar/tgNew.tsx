b1f31d920ce1f188756a4eeb07e9600fbe7053f3
DAppArtisan <141224058+DAppArtisan@users.noreply.github.com>
2024-09-03 02:14:05 +0530

    update id

--- src/components/navbar/tgNew.tsx
+++ src/components/navbar/tgNew.tsx
@@ -1,7 +1,7 @@
 import Script from "next/script";
 import { useEffect, useReducer, useRef, useState } from "react";
 const TGLogin = () => {
-  const botUsername = "communitysetupbot"; // Aapke bot ka username yahan likhein
+  const botUsername = "7280759352"; // Aapke bot ka username yahan likhein
   const authCallbackUrl = encodeURIComponent(
     "https://telegram-auth-five.vercel.app/"
   ); // Callback URL ko yahan likhein
@@ -11,11 +11,10 @@ const TGLogin = () => {
     const script = document.createElement("script");
     script.src = "https://telegram.org/js/telegram-widget.js?22";
     script.async = true;
-    script.setAttribute("data-telegram-login", "communitysetupbot");
+    script.setAttribute("data-telegram-login", "7280759352");
     script.setAttribute("data-size", "large");
     script.setAttribute("data-onauth", "onTelegramAuth(user)");
     script.setAttribute("data-request-access", "write");
-    script.async = true;
     scriptContainerRef.current?.appendChild(script);
   }, []);
 
